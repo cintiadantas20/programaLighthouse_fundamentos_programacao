02d075363ce9ffe7794563e2510f4ae49170e677
Cíntia <cintiamcdantas@gmail.com>
2023-04-19 09:04:39 -0300

    Cria o arquivo main.py

---
+++ sitechecker/__main__.py
@@ -0,0 +1,30 @@
+import sys
+import pathlib
+
+from sitechecker.cli import read_user_cli_args
+from sitechecker.cli import display_check_result
+from sitechecker.checker import site_is_online
+
+def main():
+    """Run Site Checker."""
+    user_args = read_user_cli_args()
+    urls = user_args.urls
+    if not urls:
+        print("Erro: sem URLs para analisar.", file=sys.stderr)
+        sys.exit(1)
+    _site_check(urls)
+
+def _site_check(urls):
+    for url in urls:
+        error = ""
+        try:
+            result = site_is_online(url)
+        except Exception as e:
+            result = False
+            error = str(e)
+        display_check_result(result, url, error)
+
+if __name__ == "__main__":
+    main()
+
+# python -m sitechecker -u python.org google.com indicium.tech
