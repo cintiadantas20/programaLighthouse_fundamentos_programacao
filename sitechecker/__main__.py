431e7045ccdc40a839bd06017e3e5b749c49ce2b
Cíntia <cintiamcdantas@gmail.com>
2023-04-19 20:30:08 -0300

    Altera o arquivo main para ver se é URL

--- sitechecker/__main__.py
+++ sitechecker/__main__.py
@@ -1,5 +1,6 @@
 import sys
 import pathlib
+import re
 
 from sitechecker.cli import read_user_cli_args
 from sitechecker.cli import display_check_result
@@ -17,14 +18,17 @@ def main():
 def _site_check(urls):
     for url in urls:
         error = ""
-        try:
-            result = site_is_online(url)
-        except Exception as e:
-            result = False
-            error = str(e)
-        display_check_result(result, url, error)
+        regex = re.compile("((http|https)\:\/\/)?[a-zA-Z0-9\.\/\?\:@\-=#]+\.([a-zA-Z]){2,6}([a-zA-Z0-9\.\&\/\?\:@\-=#])*")
+        valid = regex.match(url)
+        if valid:
+            try:
+                result = site_is_online(url)
+            except Exception as e:
+                result = False
+                error = "Essa página está offline ou não existe"
+            display_check_result(result, url, error)
+        else:
+            print(f'"{url}" não é uma URL válida')
 
 if __name__ == "__main__":
     main()
-
-# python -m sitechecker -u python.org google.com indicium.tech
